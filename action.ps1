34d3825d283a699737e1e21d6ec951a7597ee810
Jeroen Blaauw <72070984+JeroenBL@users.noreply.github.com>
2023-10-05 10:23:39 +0200

    updated action

--- action.ps1
+++ action.ps1
@@ -2,13 +2,13 @@
 ###########################################################
 # Form mapping
 $formObject = @{
-    MailboxDistinguishedName = $form.MailboxDistinguishedName
-    UsersToRemove            = $form.UsersToRemove.id
+    MailboxIdentity = $form.MailboxDistinguishedName
+    UsersToRemove   = $form.UsersToRemove.id
 }
 [bool]$IsConnected = $false
 
 try {
-    Write-Information "Executing ExchangeOnline action: [MailboxRevokeSendAs] for: [$($formObject.MailboxDistinguishedName)]"
+    Write-Information "Executing ExchangeOnline action: [MailboxRevokeSendAs] for: [$($formObject.MailboxIdentity)]"
 
     $null = Import-Module ExchangeOnlineManagement
 
@@ -18,31 +18,31 @@ try {
     $IsConnected = $true
 
     foreach ($user in $formObject.UsersToRemove) {
-        $null =  Remove-RecipientPermission -Identity $formObject.MailboxDistinguishedName -AccessRights SendAs -Confirm:$false -Trustee $user -ErrorAction Stop
+        $null =  Remove-RecipientPermission -Identity $formObject.MailboxIdentity -AccessRights SendAs -Confirm:$false -Trustee $user -ErrorAction Stop
 
         $auditLog = @{
             Action            = 'UpdateResource'
             System            = 'ExchangeOnline'
-            TargetIdentifier  = $formObject.MailboxDistinguishedName
-            TargetDisplayName = $formObject.MailboxDistinguishedName
-            Message           = "ExchangeOnline action: [MailboxRevokeSendAs] Revoke [$($user)] from mailbox [$($formObject.MailboxDistinguishedName)] executed successfully"
+            TargetIdentifier  = $formObject.MailboxIdentity
+            TargetDisplayName = $formObject.MailboxIdentity
+            Message           = "ExchangeOnline action: [MailboxRevokeSendAs] Revoke [$($user)] from mailbox [$($formObject.MailboxIdentity)] executed successfully"
             IsError           = $false
         }
         Write-Information -Tags 'Audit' -MessageData $auditLog
-        Write-Information "ExchangeOnline action: [MailboxRevokeSendAs] Revoke [$($user)] from mailbox [$($formObject.MailboxDistinguishedName)] executed successfully"
+        Write-Information "ExchangeOnline action: [MailboxRevokeSendAs] Revoke [$($user)] from mailbox [$($formObject.MailboxIdentity)] executed successfully"
     }
 } catch {
     $ex = $_
     $auditLog = @{
         Action            = 'UpdateResource'
         System            = 'ExchangeOnline'
-        TargetIdentifier  = $formObject.MailboxDistinguishedName
-        TargetDisplayName = $formObject.MailboxDistinguishedName
-        Message           = "Could not execute ExchangeOnline action: [MailboxRevokeSendAs] for: [$($formObject.MailboxDistinguishedName)], error: $($ex.Exception.Message)"
+        TargetIdentifier  = $formObject.MailboxIdentity
+        TargetDisplayName = $formObject.MailboxIdentity
+        Message           = "Could not execute ExchangeOnline action: [MailboxRevokeSendAs] for: [$($formObject.MailboxIdentity)], error: $($ex.Exception.Message)"
         IsError           = $true
     }
     Write-Information -Tags "Audit" -MessageData $auditLog
-    Write-Error "Could not execute ExchangeOnline action: [MailboxRevokeSendAs] for: [$($formObject.MailboxDistinguishedName)], error: $($ex.Exception.Message)"
+    Write-Error "Could not execute ExchangeOnline action: [MailboxRevokeSendAs] for: [$($formObject.MailboxIdentity)], error: $($ex.Exception.Message)"
 } finally {
     if ($IsConnected) {
         $null = Disconnect-ExchangeOnline -Confirm:$false -Verbose:$false
